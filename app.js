0ffaf2597e36e819d57d210d901405f5208ac15d
Rafael <rafaelbar37@gmail.com>
2020-02-16 23:22:27 +0200

    commit

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 var url = require('url');
-var thriller = require('./thriller');
+var devices = require('./devices');
 /////////////////
+
+app.use(cors({
+  credentials: true,
+}));
+
 app.get ('/all', function (req, res) {
-	res.json(thriller.getAllBooks());
+	res.json(devices.getDevices());
 });
 
-app.get ('/id/:bookid', function (req, res) {
-	res.json(thriller.getBookById(req.params.bookid));
+app.get ('/id/:deviceid', function (req, res) {
+	res.json(devices.getDeviceTypeById(req.params.deviceid));
 });
 
-app.get ('/reviews', function (req, res) {
-	res.json(thriller.getMostReviews());
-});
 ///////////////////
 app.listen(process.env.PORT || 3000);
 console.log('listen 3000');
